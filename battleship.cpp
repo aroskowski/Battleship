4e4684b28eedaf2fc718e1090ab6192dbc4a1dde
aroskowski <118449882+aroskowski@users.noreply.github.com>
2022-11-30 17:29:42 +0000

    PrintBoard

--- battleship.cpp
+++ battleship.cpp
@@ -25,9 +25,9 @@ int main(){
 
 void PrintBoard(){
     for(int i = 0, i <= 10; i++){
-        printf("    0   1   2   3   4   5   6   7   8   9   ")
+        printf("    0   1   2   3   4   5   6   7   8   9   ");
         for(int j = 0; j <= 10; j++){
-            printf(A)
+            printf("A");
 
         }
     }    
