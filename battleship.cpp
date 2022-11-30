4378367314cdef88aadbe701dac68b33b2d64359
aroskowski <118449882+aroskowski@users.noreply.github.com>
2022-11-30 16:58:20 +0000

    User Play

--- battleship.cpp
+++ battleship.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int main(){
     int turnNum = 0;
-    
+    char play;
+
+    printf("Would you like to play a game? (y or n)");
+    cin << play;
+    while(play == y){
+        printf("\nWelcome to the game!\n");
+        play = n;
+    }
 
     return 0;
 }
